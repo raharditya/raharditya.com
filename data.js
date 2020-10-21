502a314bab3e11f14166343f954531645f3aa339
Asebodi <aditraharditya45@gmail.com>
2020-10-21 12:34:04 +0700

    Implemented assets

--- data.js
+++ data.js
@@ -1,24 +1,58 @@
 const data = [
   {
     id: 0,
-    thumbnail: "/mockup.jpg",
+    thumbnail: "/assets/1-kkn.svg",
+    url: "https://kkncovidjogja.online",
     title: "KKN Covid Jogja",
     tags: ["React", "WebApp"],
     description: [
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      "Aplikasi berbasis website ini saya buat dalam rangka Kuliah Kerja Nyata yang harus dilakukan secara online karena terjadinya pandemi Covid-19. Project ini adalah salah satu program kerja dalam rangka mengedukasi masyarakat mengenai pandemi Covid-19 serta kebiasaan New Normal.",
+      "Dalam aplikasi berbasis website ini, terdapat informasi kasus Covid-19 dalam cakupan Provinsi Daerah Istimewa Yogyakarta sampai tiap kabupaten dan kecamatan yang ada di Provinsi DIY dan diperbarui secara otomatis setiap harinya. Selain itu juga terdapat berita terkait Covid-19 dari 5 sumber yang berbeda yang diperbarui secara otomatis setiap dua jam sekali. Ada pula berbagai artikel dan informasi hoax yang dikumpulkan secara manual oleh tim KKN.",
     ],
   },
   {
     id: 1,
-    thumbnail: "/mockup.jpg",
-    title: "Sapasampah",
+    thumbnail: "/assets/2-jadwal.svg",
+    url: "https://jadwal.raharditya.com",
+    title: "Jadwal Pemadatan",
     tags: ["Java", "Android"],
     description: [
-      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
-      "Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+      "Aplikasi berbasis website ini saya buat pada awal pandemi Covid-19. Di saat itu, terjadi berbagai kesalahan informasi di kalangan mahasiswa Administrasi Negara FISIP UNS terkait jadwal yang mana yang paling baru dan benar. Hal ini dikarenakan seringnya pergantian jadwal karena dilaksanakannya pemadatan kegiatan perkuliahan.",
+      "Aplikasi berbasis web ini memuat seluruh jadwal dari tiap semester dan kelas yang ada di Prodi AN sehingga dapat diakses berbagai angkatan mahasiswa. Project ini dibuat menggunakan React. Project ini mendapatkan hampir 2 ribu pengunjung pada saat kegiatan perkuliahan berlangsung.",
     ],
   },
+  {
+    id: 2,
+    thumbnail: "/assets/3-doolan.svg",
+    title: "Doolan",
+    tags: ["Java", "Android"],
+    description: [
+      "Project ini merupakan hasil dari perlombaan yang saya ikuti, yaitu Kompetisi Inovasi Digital yang diadakan oleh Fintech Center UNS. Ini merupakan prototype UI/UX untuk sebuah konsep aplikasi bernama Doolan. Konsep aplikasi ini berkategori pariwisata.",
+      "Konsep dari aplikasi ini adalah supaya wisatawan dapat membeli tiket untuk wahana wisata dalam bentuk paket ataupun single. Wahana wisata yang masuk dalam konsep Doolan adalah seperti Jeep Wisata, Odong-odong Alun-alun, ATV Wisata, Andong, dan lain-lain.",
+    ],
+  },
+  // {
+  //   id: 3,
+  //   thumbnail: "/assets/4-kmap.svg",
+  //   url: "https://kmap.fisip.uns.ac.id",
+  //   title: "Keluarga Mahasiswa Administrasi Publik",
+  //   tags: ["Java", "Android"],
+  //   description: [
+  //     "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
+  //     "Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+  //   ],
+  // },
+  // {
+  //   id: 4,
+  //   thumbnail: "/assets/5-admi.svg",
+  //   url: "https://archive.raharditya.com/admi",
+  //   title: "Temu AdMI 2019",
+  //   tags: ["Java", "Android"],
+  //   description: [
+  //     "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
+  //     "Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+  //   ],
+  // },
 ];
 
 export default data;
