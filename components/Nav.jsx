f94ca71083185c604816a3eeff7274147cec5a10
Asebodi <aditraharditya45@gmail.com>
2020-10-25 10:03:35 +0700

    Changed Nav to new design

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,17 +1,38 @@
 import React from "react";
+import Link from "next/link";
 import PropTypes from "prop-types";
 
 function Nav(props) {
   return (
-    <nav className="padding-both">
-      <h3>RHDT</h3>
+    <nav className="w-full h-16 bg-gray-200 flex items-center">
+      <div className="flex justify-between items-center container mx-auto">
+        <h3 className="font-extrabold text-2xl text-red-500">
+          <Link href="/">
+            <a>RHDT</a>
+          </Link>
+        </h3>
 
-      <div className="menu">
-        {/* <small>Menu</small>
-        <div className="burger">
-          <div className="line1"></div>
-          <div className="line2"></div>
-        </div> */}
+        <ul>
+          <li>
+            <Link href="/">
+              <a className="text-sm mr-4">Home</a>
+            </Link>
+
+            <Link href="#projects">
+              <a className="text-sm mr-4">Projects</a>
+            </Link>
+
+            <Link href="/contact">
+              <a className="text-sm mr-4">Contact Me</a>
+            </Link>
+          </li>
+        </ul>
+
+        <div className="burger md:hidden">
+          <div className="line1 rounded-sm h-1 w-6 my-1 bg-black"></div>
+          <div className="line2 rounded-sm h-1 w-6 my-1 bg-black"></div>
+          <div className="line2 rounded-sm h-1 w-6 my-1 bg-black"></div>
+        </div>
       </div>
     </nav>
   );
