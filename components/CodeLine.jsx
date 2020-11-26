e055da3b3b50c315e8ed1540ad76ab59dd428ebf
Asebodi <aditraharditya45@gmail.com>
2020-11-26 22:15:31 +0700

    FIxed indentation problem

--- components/CodeLine.jsx
+++ components/CodeLine.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function CodeLine(props) {
+function CodeLine({ indent = 0, line, children }) {
+  const space = <>&nbsp;</>;
+  const outputIndent = (amount) => {
+    let idt = [];
+
+    for (let i = 0; i < amount; i++) {
+      idt.push(<span key={i}>&nbsp;</span>);
+    }
+
+    return idt;
+  };
+
   return (
-    <div className="w-full  flex items-center">
-      <div className="w-8 text-center text-sm">{props.line}</div>
+    <div className="w-full flex items-center">
+      <div className="w-8 text-center text-sm">{line}</div>
       <code className="pl-4">
-        {props.indent * "&nbsp;"}
-        {props.children}
+        {outputIndent(indent)}
+        {children}
       </code>
     </div>
   );
