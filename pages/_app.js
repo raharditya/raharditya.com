2cf41150ee50b42b923fd800d02b2f3756f3d973
Asebodi <aditraharditya45@gmail.com>
2020-12-01 19:30:29 +0700

    Added global styling and import

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Nav from "../components/Nav";
 import "../styles/tailwind.css";
 import { useState } from "react";
 
+import "../styles/global.scss";
 import Footer from "../components/containers/Footer";
 
 function MyApp({ Component, pageProps }) {
