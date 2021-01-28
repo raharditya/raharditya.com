4e27b5393429bc3e7ccc037d1529bc4db972c973
Asebodi <aditraharditya45@gmail.com>
2021-01-28 10:50:57 +0700

    useMediaQuery is now passed into every page as props

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,18 @@
 import Nav from "../components/Nav";
 import "../styles/tailwind.css";
 import { useState } from "react";
+import { useMediaQuery } from "../components/useMediaQuery";
 
 import "../styles/global.scss";
 import Footer from "../components/containers/Footer";
 
 function MyApp({ Component, pageProps }) {
+  const isMobile = useMediaQuery("(max-width: 768px)");
+
   return (
     <>
       <Nav />
-      <Component {...pageProps} />
+      <Component {...pageProps} isMobile={isMobile} />
       <Footer />
     </>
   );
--- pages/projects.jsx
+++ pages/projects.jsx
@@ -1,16 +1,34 @@
 import ProjectItem from "../components/ProjectItem";
 
 function projects(props) {
+  if (process.browser) {
+    console.log(props.isMobile);
+  }
   return (
-    <div className="pt-24">
-      <div className="relative px-6 mb-40">
+    <div className="pt-24 md:container md:mx-auto md:px-2">
+      <div className="relative px-6 mb-40 md:mb-56">
         <h2 className="relative z-10 text-heading-serif page-heading font-bold text-4xl">
           Projects
         </h2>
 
         <div
           className="absolute z-0 bg-gray-400 w-8/12 rounded-xl"
-          style={{ left: "5rem", top: "-0.5rem", height: "9rem" }}
+          style={
+            props.isMobile
+              ? {
+                  left: "5rem",
+                  right: "auto",
+                  top: "-0.5rem",
+                  height: "9rem",
+                }
+              : {
+                  left: "auto",
+                  right: "0px",
+                  top: "-0.5rem",
+                  height: "14rem",
+                  width: "90%",
+                }
+          }
         ></div>
       </div>
 
