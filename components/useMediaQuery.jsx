183973c0072ee354b25bf2fd34ebf04cfc8b13a7
Asebodi <aditraharditya45@gmail.com>
2021-01-28 10:50:17 +0700

    Run useMediaQuery only on client side

--- components/useMediaQuery.jsx
+++ components/useMediaQuery.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
 
 export const useMediaQuery = (query) => {
-  const mediaMatch = window.matchMedia(query);
-  const [matches, setMatches] = useState(mediaMatch.matches);
+  if (process.browser) {
+    const mediaMatch = window.matchMedia(query);
+    const [matches, setMatches] = useState(mediaMatch.matches);
 
-  useEffect(() => {
-    const handler = (e) => setMatches(e.matches);
-    mediaMatch.addListener(handler);
-    return () => mediaMatch.removeListener(handler);
-  });
-  return matches;
+    useEffect(() => {
+      const handler = (e) => setMatches(e.matches);
+      mediaMatch.addListener(handler);
+      return () => mediaMatch.removeListener(handler);
+    });
+    return matches;
+  }
 };
