0e55bd94c3122e13598cd3719dafab560280a122
Asebodi <aditraharditya45@gmail.com>
2021-07-18 23:49:12 +0700

    Installed react slick

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,8 @@ import { useMediaQuery } from "../components/useMediaQuery";
 
 import "../styles/global.scss";
 import Footer from "../components/containers/Footer";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function MyApp({ Component, pageProps }) {
   const isMobile = useMediaQuery("(max-width: 768px)");
