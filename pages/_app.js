47d4a6a2812b7e917096be498877009867690c75
Asebodi <aditraharditya45@gmail.com>
2021-07-26 08:30:30 +0700

    Implemented scroll to top button

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import { useMediaQuery } from "../components/useMediaQuery";
 
 import "../styles/global.scss";
 import Footer from "../components/containers/Footer";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
   const isMobile = useMediaQuery("(max-width: 768px)");
@@ -14,6 +15,18 @@ function MyApp({ Component, pageProps }) {
       <Nav />
       <Component {...pageProps} isMobile={isMobile} />
       <Footer />
+      <Link href="#top">
+        <a
+          className="scroll-to-top fixed p-4 rounded-md shadow-lg bg-white z-20"
+          style={{ bottom: "2rem", right: "2rem" }}
+        >
+          <img
+            src="/assets/scroll-to-top.svg"
+            alt=""
+            className="w-6 transform transition duration-150 ease-in-out"
+          />
+        </a>
+      </Link>
     </>
   );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -4,6 +4,8 @@ import HomeHeader from "../components/HomeHeader";
 import Stack from "../components/containers/Stack";
 import Projects from "../components/containers/Projects";
 import Invitation from "../components/Invitation";
+import HomeIntroduction from "../components/HomeIntroduction";
+import FeaturedProject from "../components/FeaturedProject";
 
 export default function Home() {
   return (
@@ -15,10 +17,39 @@ export default function Home() {
 
       <div className="project-container">
         <HomeHeader />
+        <HomeIntroduction />
         <Stack />
+        <FeaturedProject />
         <Projects />
         <Invitation />
       </div>
+
+      {/* <div className="fixed p-4" style={{ left: "2rem", top: "4rem" }}>
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-green-400 p-2 rounded-md block h-full mb-4"
+        >
+          <img src="/assets/social/instagram.svg" alt="" />
+        </a>
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-green-400 p-2 rounded-md block h-full mb-4"
+        >
+          <img src="/assets/social/github.svg" alt="" />
+        </a>
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-green-400 p-2 rounded-md block h-full"
+        >
+          <img src="/assets/social/linkedin.svg" alt="" />
+        </a>
+      </div> */}
     </div>
   );
 }
