e87d04eb1a03ecee0e560d1732a12badfc461378
Asebodi <aditraharditya45@gmail.com>
2021-07-27 17:16:41 +0700

    Fixed mobile nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,6 +5,7 @@ import { NavLink } from "./NavLink";
 
 function Nav(props) {
   const [showBg, setShowBg] = useState(false);
+  const [navOpen, setNavOpen] = useState(false);
 
   useScrollPosition(({ prevPos, currPos }) => {
     if (currPos.y < -200) {
@@ -29,9 +30,18 @@ function Nav(props) {
           </Link>
         </h3>
 
-        <ul className="items-center gap-12 hidden md:flex">
+        <ul
+          className={`items-center gap-8 md:gap-12 md:flex ${
+            navOpen ? "nav-mobile-open" : ""
+          }`}
+        >
           <li>
-            <NavLink href="/" exact className="text-light text-sm">
+            <NavLink
+              href="/"
+              exact
+              className="text-light text-2xl md:text-sm"
+              onClick={() => setNavOpen(false)}
+            >
               Home
             </NavLink>
             {/* <Link href="/">
@@ -39,7 +49,11 @@ function Nav(props) {
             </Link> */}
           </li>
           <li>
-            <NavLink href="/projects" className="text-light text-sm">
+            <NavLink
+              href="/projects"
+              className="text-light text-2xl md:text-sm"
+              onClick={() => setNavOpen(false)}
+            >
               Projects
             </NavLink>
             {/* <Link href="/projects">
@@ -47,19 +61,29 @@ function Nav(props) {
             </Link> */}
           </li>
           <li>
-            <NavLink href="/contact-me" exact className="text-light text-sm">
+            <NavLink
+              href="/contact-me"
+              exact
+              className="text-light text-2xl md:text-sm"
+              onClick={() => setNavOpen(false)}
+            >
               Contact Me
             </NavLink>
             {/* <Link href="/contact-me">
               <a className="text-light text-sm">Contact Me</a>
             </Link> */}
           </li>
+
+          <h2 className="mt-24 font-bold md:hidden">raharditya.com</h2>
         </ul>
 
-        <div className="burger md:hidden">
+        <div
+          className={`burger md:hidden ${navOpen ? "toggle" : ""}`}
+          onClick={() => setNavOpen(!navOpen)}
+        >
           <div className="line1 rounded-sm h-1 w-6 my-1 bg-black"></div>
           <div className="line2 rounded-sm h-1 w-6 my-1 bg-black"></div>
-          <div className="line2 rounded-sm h-1 w-6 my-1 bg-black"></div>
+          <div className="line3 rounded-sm h-1 w-6 my-1 bg-black"></div>
         </div>
       </div>
     </nav>
