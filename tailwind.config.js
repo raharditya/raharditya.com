72518afa46dcaa32a6255d212e50b47af169a6ae
Asebodi <aditraharditya45@gmail.com>
2021-10-15 12:11:15 +0700

    Added purge paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   purge: [
     "./components/**/*.{js,jsx,ts,tsx,svg}",
+    "./components/containers/**/*.{js,jsx,ts,tsx,svg}",
+    "./components/projects/**/*.{js,jsx,ts,tsx,svg}",
     "./pages/**/*.{js,jsx,ts,tsx,svg}",
   ],
   theme: {},
