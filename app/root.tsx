ac2fe2d61b123723d580161d31c8d98f6dc3c962
raharditya <raharditya@skilvul.com>
2024-08-26 13:31:13 +0700

    Potentially fix google analytics

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,7 @@
+import { useEffect } from 'react';
 import { LinksFunction, LoaderFunction } from '@remix-run/node';
-import { Links, Meta, Outlet, Scripts, ScrollRestoration, json, useLoaderData } from '@remix-run/react';
+import { Links, Meta, Outlet, Scripts, ScrollRestoration, json, useLoaderData, useLocation } from '@remix-run/react';
+import * as gtag from 'app/lib/utils/gtag.client';
 
 import tailwind from './styles/tailwind.css?url';
 import global from './styles/global.css?url';
@@ -17,14 +19,18 @@ export const links: LinksFunction = () => {
 
 export const loader: LoaderFunction = async () => {
   return json({
-    ENV: {
-      GTAG_ID: process.env.GTAG_ID,
-    },
+    gtagId: process.env.GTAG_ID,
   });
 };
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const location = useLocation();
   const data = useLoaderData<typeof loader>();
+
+  useEffect(() => {
+    if (!data.gtagId) return;
+    gtag.pageview(location.pathname, data.gtagId);
+  }, [location.pathname, data.gtagId]);
 
   return (
     <html lang="en">
@@ -34,15 +40,18 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
         {/* <!-- Google tag (gtag.js) --> */}
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${data.ENV.GTAG_ID}`}></script>
-        <script>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${data.ENV.GTAG_ID}');
-          `}
-        </script>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${data.gtagId}`}></script>
+        <script
+          async
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${data.gtagId}');
+            `,
+          }}
+        />
       </head>
       <body className="font-jakarta">
         {children}
