fc3f25f0b3228e0bb8b2384ff364653acbcf6211
raharditya <raharditya@skilvul.com>
2024-08-26 11:55:15 +0700

    Implement gtag and update favicon

--- app/root.tsx
+++ app/root.tsx
@@ -1,17 +1,31 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import { LinksFunction, LoaderFunction } from '@remix-run/node';
+import { Links, Meta, Outlet, Scripts, ScrollRestoration, json, useLoaderData } from '@remix-run/react';
 
 import tailwind from './styles/tailwind.css?url';
 import global from './styles/global.css?url';
-import { LinksFunction } from '@remix-run/node';
 
 export const links: LinksFunction = () => {
   return [
     { rel: 'stylesheet', href: tailwind },
     { rel: 'stylesheet', href: global },
+    { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+    { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+    { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+    { rel: 'manifest', href: '/site.webmanifest' },
   ];
 };
 
+export const loader: LoaderFunction = async () => {
+  return json({
+    ENV: {
+      GTAG_ID: process.env.GTAG_ID,
+    },
+  });
+};
+
 export function Layout({ children }: { children: React.ReactNode }) {
+  const data = useLoaderData<typeof loader>();
+
   return (
     <html lang="en">
       <head>
@@ -19,10 +33,25 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${data.ENV.GTAG_ID}`}></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${data.ENV.GTAG_ID}');
+          `}
+        </script>
       </head>
       <body className="font-jakarta">
         {children}
         <ScrollRestoration />
+        {/* <script
+          dangerouslySetInnerHTML={{
+            __html: `window.ENV = ${JSON.stringify(data.ENV)}`,
+          }}
+        /> */}
         <Scripts />
       </body>
     </html>
